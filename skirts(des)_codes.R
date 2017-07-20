c509091c5e437ac793c05fe7d132bb318d4330f8
Atul Kumar Sharma <17nov.atul@gmail.com>
2017-07-20 21:49:54 +0530

    Update skirts(des)_codes.R

--- skirts(des)_codes.R
+++ skirts(des)_codes.R
@@ -16,7 +16,7 @@ library(forecast)
 R_future = forecast:::forecast.HoltWinters(R_forecast, h=5)
 #Plotting forecast for next 5 periods
 plot(R_future)
-#Checking for qnorm
+#Checking residuals for accuracy
 qqnorm(R_future$residuals) #Everything seems good here as residuals have stayed on a straight line along the diagonal
 plot(R_future$residuals) #Everything seems good here as not more than spike has crossed the significance margin
 hist (R_future$residuals) #Everything seems good here as residuals are following close to normal distribution
